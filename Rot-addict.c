67405f533acbbc3ba94c52cfbe6827d299261b3a
soheiworks <soheiworks@proton.me>
2025-10-02 22:29:19 +0900

    ■が落下するように変更

--- Rot-addict.c
+++ Rot-addict.c
@@ -1,22 +1,49 @@
-#include <stdio.h> // 標準入出力ライブラリ
-              
-int main(void){
-    int piece; 
+#include <stdio.h>
+#ifdef _WIN32
+ #include <windows.h>
+#else
+ #include <unistd.h>
+#endif
 
-    printf("■");
+int main(void){
+    int piece;
+    int row;
+    int rows;
+    int col;
+    int frame_ms;
+
+    rows = 12;
+    col = 10;
+    frame_ms = 120;
+
+    printf("\x1b[?25l");
     fflush(stdout);
 
-piece = getchar();
-printf("\x1b[G");
-printf("\x1b[2K");
+    for(row = 1; row <= rows; row++){
 
-printf("\x1b[F");
-printf("\x1b[2K");
-printf("\x1b[G");
+        printf("\x1b[2J");
+        printf("\x1b[H");
+        printf("\x1b[%d;%dH",row,col);
+        printf("■");
+        fflush(stdout);
 
-fflush(stdout);
-printf("　");
-fflush(stdout);
-getchar();
-return 0;
+        #ifdef _WIN32
+            Sleep(frame_ms);
+        #else
+            usleep(frame_ms * 1000);
+        #endif
+   }
+
+    printf("\x1b[2J");
+    printf("\x1b[H");
+    printf("\x1b[?25h");
+
+    printf("落下デモ終了。Enterで終了します。\n");
+    fflush(stdout);
+    piece = getchar();
+    (void)piece;
+
+    return 0;
+
 }
+
